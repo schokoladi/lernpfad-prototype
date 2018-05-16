bf3be2dd1987dc0aaa94e4771308bb46f48582d7
Dominik Schoch <schokoladi@protonmail.com>
2018-05-16 14:35:41 +0200

    added static json data

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,34 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+  <div class="app">
+    <side-nav v-bind:activities="activities"></side-nav>
   </div>
 </template>
 
 <script>
+import SideNav from './components/SideNav'
+import axios from 'axios'
+
 export default {
-  name: 'App'
+  components: {
+    'side-nav': SideNav
+  },
+  data () {
+    return {
+      activities: [],
+      title: 'Activities'
+    }
+  },
+  methods: {
+    getActivities: function () {
+      axios.get('static/activities.json')
+        .then(response => {
+          this.activities = response.data
+        })
+    }
+  },
+  mounted () {
+    this.getActivities()
+  }
 }
 </script>
 
---
+++ src/components/SideNav.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="activities">
+    <h1>Activities</h1>
+    <ul>
+      <li v-for="activity in activities" v-bind:key="activity.id">
+        <h2>{{ activity.userName }}</h2>
+        <h3>{{ activity.comment }}</h3>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    activities: {
+      type: Array,
+      required: true
+    }
+  },
+  data () {
+    return {
+    }
+  }
+}
+</script>
